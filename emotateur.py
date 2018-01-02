6b565bfcd58cf45aec6cfebdbaa7a3a472aefb98
zhangheng19931123 <zhangheng19931123@gmail.com>
2018-01-02 16:58:40 +0100

    add test images

--- emotateur.py
+++ emotateur.py
@@ -16,12 +16,17 @@ def run():
     actual_fps = 0
     paused = False
     delay = {True: 0, False: 1}
-    faceBB = [150, 80, 300, 300]
+    faceBB = [50, 50, 400, 400]
 
-    img_reference = cv2.imread('img/test2.jpg')
+    img_reference_file_name = 'img/test1.jpg'
+    img_reference = cv2.imread(img_reference_file_name)
+    if 'test1' in img_reference_file_name:
+        faceBB = [180, 50, 300, 300]
+    elif 'test2' in img_reference_file_name:
+        faceBB = [180, 60, 250, 250]
     res_reference = cv2.resize(img_reference,(frame.shape[1],frame.shape[0]), interpolation = cv2.INTER_CUBIC)
     res_reference, face_key_points_reference = fc.get_face_key_points(res_reference, faceBB)
-    cv2.imshow("img", res_reference)
+    cv2.imshow("reference", res_reference)
 
     while True:
         start_time = time.time()
@@ -36,9 +41,7 @@ def run():
         res, face_key_points = fc.get_face_key_points(frame, faceBB)
         cv2.putText(res, 'Press \'q\' to stop.', (20, 20), 0, 0.5, (0, 0, 255))
         cv2.putText(res, 'Press \'p\' to pause.', (20, 40), 0, 0.5, (0, 0, 255))
-        cv2.imshow("OpenPose result", res)
-
-        #print('face_key_points={}'.format(face_key_points))
+        cv2.imshow("webcam", res)
 
         key = cv2.waitKey(delay[paused])
         if key & 255 == ord('p'):
